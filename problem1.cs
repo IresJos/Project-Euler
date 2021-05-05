2b5b57bdd98b0aa30a13ca0e5b8c61f80eb83c70
IresJos <76781974+IresJos@users.noreply.github.com>
2021-05-05 14:22:44 +0200

    Add files via upload

---
+++ problem1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MultiplesOf3and5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totaal = 0;
+            for (int i = 1; i < 334; i++)
+            {
+                totaal += (i * 3);
+                Console.WriteLine(i + " " + totaal + " " + i * 3);
+            }
+          
+            for (int i = 1; i < 200; i++)
+            {
+                if ((i * 5) % 3 != 0)
+                {
+                    totaal += (i * 5);
+                }
+                Console.WriteLine(i + " " + totaal + " " + i * 5);
+            }
+            
+            Console.WriteLine(totaal);
+        }
+
+    }
+}
