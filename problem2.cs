05e5154de8d93c0a18145a02fed0af851c95e1b4
IresJos <76781974+IresJos@users.noreply.github.com>
2021-05-05 14:55:52 +0200

    Add files via upload

---
+++ problem2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cijfer = 0;
+            int nwTotaal = 1;
+            int oudTotaal = 1;
+            int som = 0;
+            long viermiljoen = 4000000;
+            nwTotaal = cijfer + oudTotaal;
+            
+            while (som < viermiljoen)
+            {
+                nwTotaal = cijfer + oudTotaal;
+                oudTotaal = cijfer;
+                cijfer = nwTotaal;
+                Console.WriteLine("nwTotaal is: " + nwTotaal);
+                if(nwTotaal % 2 == 0)
+                {
+                    som += nwTotaal;
+                    Console.WriteLine("totaal som is: " + som);
+                }
+
+            }
+        }
+    }
+}
